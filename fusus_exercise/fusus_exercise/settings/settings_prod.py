18b526b9bf274adf1eafbaba411c0a64d170408a
visheshvvs <visheshvvs@gmail.com>
2021-07-19 22:19:15 -0400

    add settings for prod base and dev

---
+++ fusus_exercise/fusus_exercise/settings/settings_prod.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-9d6@r6@toez%gb*rz$@=3hmcsmpr=io70hw$+frf=j7t&gbr@e'
+
+ALLOWED_HOSTS = ['website_name.com']
+DEBUG = False
